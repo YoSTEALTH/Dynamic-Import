2f2420916e4bb036341828c113e8aff37049ea22
YoSTEALTH <ritesh@stealthcentral.com>
2018-08-30 14:59:12 -0600

    added new sub-dir examples

--- example/example.py
+++ example/example.py
@@ -1,12 +1,16 @@
+# flake8: noqa
+
 # Static Import #1
 # ----------------
 from sample import static
+# Only "sample" & "sample.static" modules are loaded at this point.
 print(static())
 print()
 
 # Dynamic Import #1
 # -----------------
-from sample import a, b, c  # noqa
+from sample import a, b, c
+# Now "sample", "sample.direct" & "sample.one" modules are loaded at this point.
 print(a())
 print(b())
 print(c())
@@ -14,7 +18,8 @@ print()
 
 # Dynamic Import #2
 # -----------------
-from sample import x, y, z  # noqa
+from sample import x, y, z
+# All "sample", "sample.direct", "sample.one" & "sample.two" modules are loaded.
 print(x())
 print(y())
 print(z())
@@ -22,5 +27,23 @@ print()
 
 # Dynamic Import #3
 # -----------------
-from sample import internals  # noqa
+from sample import internals
+# This is to demonstrate you can relatively import one module from another module.
 print(internals())
+print()
+
+# Sub-page Import #1
+# ------------------
+from sample import e, f, g
+# This demonstrates you can use nested sub-dir and use main module to import from.
+print(e())
+print(f())
+print(g())
+print()
+
+# Sub-page Import #2
+# ------------------
+from sample import name
+# Another sub-dir example
+print(name())
+print()
