62ae9d64c4ee0b4a7416fe5f1828cfee278a4c25
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-10 16:37:01 -0600

    added datestamp to auto generate version from now on, also updated setup() info to production ready.

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 from setuptools import setup, find_packages
+from datestamp import stamp
+
 
 with open('README.rst', 'r') as file:
     long_description = file.read()
 
-setup(
-    name='dynamic_import',
-    author='STEALTH',
-    version='0.9.7',
-    author_email='ritesh@stealthcentral.com',
-    description="Dynamically/Lazily import python module on run-time. \
-    Also enables easy to use import path name.",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    python_requires='>=3.6',
-    packages=find_packages(),
-    url='https://github.com/YoSTEALTH/Dynamic-Import',
-    classifiers=[
-        # Info: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: Public Domain',
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
-)
+project = 'dynamic_import'
+
+setup(url='https://github.com/YoSTEALTH/Dynamic-Import',
+      name=project,
+      author='STEALTH',
+      version=stamp(project),
+      packages=find_packages(),
+      description=('Dynamically/Lazily import python module on run-time. '
+                   'Also enables easy to use import path name.'),
+      python_requires='>=3.6',
+      long_description=long_description,
+      long_description_content_type="text/x-rst",
+      # Info: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['License :: Public Domain',
+                   # 'Development Status :: 4 - Beta',
+                   # note: had 0 issues reported over the year, should be ready for production.
+                   'Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Topic :: Software Development :: Libraries :: Python Modules'])
