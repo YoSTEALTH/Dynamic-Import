6c3a385a530fb7a5c70e73fb7ba0e95fd85265f6
YoSTEALTH <ritesh@stealthcentral.com>
2024-01-31 18:21:42 -0600

    test: improved `test_prep()` and created `test_prep_variables()`

--- test/prep_test.py
+++ test/prep_test.py
@@ -1,4 +1,5 @@
-from dynamic_import.prep import EXT_SUFFIX, prep_package
+import pytest
+from dynamic_import.prep import EXT_SUFFIX, prep_package, prep_variables
 
 
 def test_cache():
@@ -11,6 +12,8 @@ def test_cache():
 
 
 def test_prep():
+    # recusvie True test
+    # ------------------
     match = {
              'DEFINE': ('basic',
                         'test/basic/__init__.py',
@@ -42,16 +45,58 @@ def test_prep():
                      'test/basic/sub/auto_find.py',
                      ['my_function', 'var', 'MyClass', 'my_async_func'])
              }
-    find_dir_mtime = {'test/basic/', 'test/basic/sub/', 'test/basic/sub/conflict/', 'test/basic/sub/child/'}
+    find_dir_mtime = {'test/basic/',
+                      'test/basic/so/',
+                      'test/basic/sub/',
+                      'test/basic/sub/conflict/',
+                      'test/basic/sub/child/'}
     cached_match = {}
+    recursive = True
     pkg_path = 'test/basic/'
 
     exclude_file = ['test/basic/sub/skip-me.py']
     exclude_dir = ['test/basic/skip/']
 
-    info, dir_mtime = prep_package('basic', pkg_path, True, exclude_file, exclude_dir)
+    info, dir_mtime = prep_package('basic', pkg_path, recursive, exclude_file, exclude_dir)
     for k, v in info.items():
         cached_match[k] = v[0:3]  # note: ignore the last file modification time.
 
     assert set(dir_mtime) == find_dir_mtime
     assert cached_match == match
+
+    # recusvie False test
+    # -------------------
+    find_dir_mtime = {'test/basic/'}
+    recursive = False
+    match = {
+            'DEFINE': ('basic',
+                       'test/basic/__init__.py',
+                       ('DEFINE',
+                        're_import')),
+            'one': ('basic.one',
+                    'test/basic/one.py',
+                    ('one',)),
+            're_import': ('basic',
+                          'test/basic/__init__.py',
+                          ('DEFINE',
+                           're_import')),
+            'two': ('basic.two',
+                    'test/basic/two.py',
+                    ('two',))
+           }
+    cached_match = {}
+    info, dir_mtime = prep_package('basic', pkg_path, recursive, exclude_file, exclude_dir)
+    for k, v in info.items():
+        cached_match[k] = v[0:3]  # note: ignore the last file modification time.
+    assert cached_match == match
+    assert set(dir_mtime) == find_dir_mtime
+
+
+def test_prep_variables(tmpdir):
+    pkg_path = tmpdir / 'no_pkg'
+    pkg_path.mkdir()
+    file_so = pkg_path / f'file{EXT_SUFFIX[1]}'
+    file_so.write(b'')
+    with pytest.raises(ModuleNotFoundError, match="No module named 'no_pkg'"):
+        list(prep_variables('no_pkg', str(file_so)))
+    assert list(prep_variables('pkg', 'file.bad')) == []
